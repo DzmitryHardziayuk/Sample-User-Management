9a91e24a3624e6027a3c47fd6b5fc41ac9c6904d
Aliaksandr-Fedasiuk <Aliaksandr_Fedasiuk@epam.com>
2017-02-15 20:10:58 +0300

    test context

--- dao/src/main/java/com/epam/test/dao/UserDaoImpl.java
+++ dao/src/main/java/com/epam/test/dao/UserDaoImpl.java
@@ -1,11 +1,23 @@
 package com.epam.test.dao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import javax.sql.DataSource;
 import java.util.List;
 
 /**
  * Dao implementation.
  */
 public class UserDaoImpl implements UserDao {
+
+    private JdbcTemplate jdbcTemplate;
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public UserDaoImpl(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+        namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    }
 
     @Override
     public List<User> getAllUsers() {
---
+++ dao/src/test/java/com/epam/test/dao/UserDaoImplTest.java
@@ -0,0 +1,32 @@
+package com.epam.test.dao;
+
+import com.epam.test.dao.UserDao;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:test-spring-dao.xml"})
+public class UserDaoImplTest{
+
+    @Autowired
+    UserDao userDao;
+
+    @Test
+    public void getAllUsersTest() throws Exception {
+
+        List<User> users = userDao.getAllUsers();
+        //assertTrue(users.size() == 2);
+    }
+}
